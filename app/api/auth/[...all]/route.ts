f815f148e6aece33c72c699ae3bbaa1abacfb3f8
Mohammed Afnan <afnankk9995@gmail.com>
2026-01-01 13:46:09 +0530

    change better auth

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,5 +1,12 @@
+// app/api/auth/[...all]/route.ts
 import { auth } from "@/lib/auth";
 
 export const runtime = "nodejs";
+
 const handler = auth.handler;
-export {handler as GET,handler as POST} 
+
+export {
+  handler as GET,
+  handler as POST,
+  handler as OPTIONS, //  REQUIRED FOR PRODUCTION
+};
